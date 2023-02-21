4960a3faf120834c7b7f311bc07978b873fed26a
Amsidh Lokhande <amsidhmicroservice@gmail.com>
2023-02-21 13:50:14 +0530

    Initial commit

---
+++ src/main/java/com/amsidh/mvc/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.amsidh.mvc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@Slf4j
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("Inside hello method of HelloController class");
+        return "Hello World";
+    }
+}
